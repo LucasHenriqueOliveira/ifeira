9e796a47a19fdeef97cadb91c60635724c3b2e96
Lucas <lucashen@gmail.com>
2020-04-07 15:46:26 -0300

    add files shared

---
+++ src/pages/shared/Header.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import logoImg from '../../assets/logo.png'; 
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    logo: {
+        width: 120,
+    }
+}));
+
+export default function Header() {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <Link to="/">
+                        <img src={logoImg} className={classes.logo} alt="Feira" />  
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+  );
+}
