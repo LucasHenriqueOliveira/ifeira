5b11dd8766824cf346b5e67de34149d283d65902
Lucas <lucashen@gmail.com>
2020-04-11 11:00:06 -0300

    add uf and city

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiCornerDownRight } from 'react-icons/fi';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import api from '../../services/api';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.png';
@@ -21,29 +25,69 @@ const useStyles = makeStyles((theme) => ({
             color: 'transparent'
        }
     },
+    select: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        marginRight: 10,
+        marginLeft: 0,
+        marginBottom: 0,
+        marginTop: 3,
+    }
 }));
 
 export default function Home() {
-   const [id, setId] = useState('');
-   const history = useHistory();
-   const classes = useStyles();
+    const [neighborhood, setNeighborhood] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+    const history = useHistory();
+    const classes = useStyles();
+    const [cities, setCities] = useState([]);
+    const [neighborhoods, setNeighborhoods] = useState([]);
 
-   async function handleSearch(event) {
-      event.preventDefault();
+    const handleChangeUf = (uf) => {
+        setUf(uf);
+        
+        api.get('cidades/' + uf).then(response => {
+            setCities(response);
+        });
 
-      try {
-         const response = await api.post('sessions', { id });
+        //setCities([{'id': 1, 'name': 'Belo Horizonte'}, {'id': 2, 'name': 'São Paulo'}]);
+    };
 
-         localStorage.setItem('ongId', id);
-         localStorage.setItem('ongName', response.data.name);
+    const handleChangeCity = (city) => {
+        setCity(city);
+        
+        api.get('bairros/' + city).then(response => {
+            setNeighborhoods(response);
+        });
 
-         history.push('/profile');
-      } catch(error) {
-         alert(error);
-      }
-   }
+        //setNeighborhoods([{'id': 1, 'name': 'Centro'}, {'id': 2, 'name': 'Santo Antônio'}]);
+    };
 
-   return (
+
+    async function handleSearch(event) {
+        event.preventDefault();
+
+        try {
+            await api.get('feirantes/' + neighborhood).then(response => {
+                if(response.length) {
+                    // save list on localstorage
+                    history.push('/list');
+                } else {
+                    console.log('Não tem bairro.');
+                }
+            });
+        } catch(error) {
+            alert(error);
+        }
+    }
+
+    const states = [
+        'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
+        'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+    ];
+
+    return (
         <div className="top_container">
             <section className="hero_section">
                 <div className="hero-container container">
@@ -57,13 +101,56 @@ export default function Home() {
                         </p>
                         <div className="hero_btn-container">
                             <form className={classes.root} onSubmit={handleSearch}>
-                                <TextField
-                                    className="neighborhood"
-                                    label="Digite o seu bairro"
-                                    variant="outlined"
-                                    value={id}
-                                    onChange={e => setId(e.target.value)}
-                                />
+                                <Grid container className={classes.root} spacing={1}>
+                                    <Grid item xs={12} sm={2}>
+                                        <FormControl variant="outlined" className={classes.select}>
+                                            <InputLabel id="labelUF">UF</InputLabel>
+                                            <Select
+                                                labelId="labelUF"
+                                                value={uf}
+                                                onChange={e => handleChangeUf(e.target.value)}
+                                            >
+                                                {states.map((state) => (
+                                                <MenuItem key={state} value={state}>
+                                                    {state}
+                                                </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12} sm={10}>
+                                        <FormControl variant="outlined" className={classes.select}>
+                                            <InputLabel id="labelCity">Cidade</InputLabel>
+                                            <Select
+                                                labelId="labelCity"
+                                                value={city}
+                                                onChange={e => handleChangeCity(e.target.value)}
+                                            >
+                                                {cities.map((city) => (
+                                                <MenuItem key={city.id} value={city.id}>
+                                                {city.name}
+                                                </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <FormControl variant="outlined" className={classes.select}>
+                                            <InputLabel id="labelNeighborhood">Bairro</InputLabel>
+                                            <Select
+                                                labelId="labelNeighborhood"
+                                                value={neighborhood}
+                                                onChange={e => setNeighborhood(e.target.value)}
+                                            >
+                                                {neighborhoods.map((neighborhood) => (
+                                                <MenuItem key={neighborhood.id} value={neighborhood.id}>
+                                                    {neighborhood.name}
+                                                </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
                                 <Link className="call_to-btn btn_white-border" to="/">
                                     <span>Pesquisar</span>
                                 </Link>
